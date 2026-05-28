46ae63932fee8a6b85e61db82c137d9fbdaceff1
LexSgz <a01709753@tec.mx>
2026-05-27 22:02:17 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "cliente.h"
 #include "entrenador.h"
+#include "pago.h"
 
 int main(){
 
@@ -21,18 +22,28 @@ int main(){
     Persona* personas[2];
 
     personas[0]=&clienteUno;
-
     personas[1]=&entrenadorUno;
 
-    for(int indice=0;
-        indice<2;
-        indice++){
+    for(int i=0;i<2;i++){
 
-        personas[indice]
-            ->mostrarInformacion();
+        personas[i]
+        ->mostrarInformacion();
 
         cout<<endl;
     }
+
+    Pago pagoCliente;
+
+    pagoCliente
+        .procesarPago(1000);
+
+    cout<<endl;
+
+    pagoCliente
+        .procesarPago(
+            1500,
+            "Tarjeta"
+        );
 
     return 0;
 }
