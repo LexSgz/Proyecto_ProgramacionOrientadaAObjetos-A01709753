7b0ee6f9bb698b488cf62ca4bdf48a72ef929219
LexSgz <a01709753@tec.mx>
2026-06-09 16:29:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,10 @@
-#include "alumno.h"
-#include "instructor.h"
-#include "pago.h"
+#include "administracion.h"
 
 int main() {
 
-    Alumno alumnoUno(
-        "Alex",
-        19,
-        101,
-        500
-    );
+    Administracion sistema;
 
-    Instructor instructorUno(
-        "Carla",
-        28,
-        202,
-        "Ballet",
-        18000
-    );
-
-    // Polimorfismo:
-    // Se almacenan objetos diferentes
-    // utilizando apuntadores a Persona.
-
-    Persona* personas[2];
-
-    personas[0] = &alumnoUno;
-    personas[1] = &instructorUno;
-
-
-    for(int i = 0; i < 2; i++) {
-
-        personas[i]
-            ->mostrarInformacion();
-
-        cout << endl;
-    }
-
-
-    Pago pagoAlumno;
-
-    pagoAlumno.procesarPago(
-        1000
-    );
-
-    cout << endl;
-
-    pagoAlumno.procesarPago(
-        1500,
-        "Tarjeta"
-    );
+    sistema.iniciarSistema();
 
     return 0;
 }
